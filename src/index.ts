fdf7b05f1b3eda0f84332d206bfc3a6d3a574d9f
Alex Eden <alexandereden91@gmail.com>
2018-12-19 02:51:52 -0600

    Implement the PWM class for initial testing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 // import { getPins } from 'raspi-board';
-import * as i2c from 'i2c-bus';
+import { PWM } from './pwm';
+// import * as i2c from 'i2c-bus';
 
-const bus = i2c.openSync(1);
-console.log(bus.scanSync());
-
+// const bus = i2c.openSync(1);
+// console.log(bus.scanSync());
+const pwm = new PWM();
+console.log(pwm);
 // console.log('Pins: ', JSON.stringify(getPins(), null, 2));
