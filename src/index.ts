13b4fe9595f7adea2543e6e2ff20ffb8f97af19a
Alex Eden <alexandereden91@gmail.com>
2018-12-18 23:19:57 -0600

    Yay address scanning works

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-import { getPins } from 'raspi-board';
+// import { getPins } from 'raspi-board';
+import * as i2c from 'i2c-bus';
 
-console.log('Pins: ', JSON.stringify(getPins(), null, 2));
+const bus = i2c.openSync(1);
+console.log(bus.scanSync());
+
+// console.log('Pins: ', JSON.stringify(getPins(), null, 2));
